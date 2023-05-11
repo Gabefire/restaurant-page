bef196cc6fa8dfa2b4ab949ee830773e4bade155
Gabe <33893811+Gabefire@users.noreply.github.com>
2023-05-11 04:48:45 -0500

    finished home page

--- src/home-page.js
+++ src/home-page.js
@@ -1,4 +1,6 @@
-import image from "./pizza.jpeg";
+import locationIcon from "./map-marker.svg";
+import timeIcon from "./clock.svg";
+import "./style.css";
 
 export default function makeHomePage() {
   const headingText = "Italian Town";
@@ -14,22 +16,19 @@ export default function makeHomePage() {
   const list = document.createElement("ul");
   const homeLi = document.createElement("li");
   const homeBtn = document.createElement("button");
-  homeBtn.id = "homeBtn";
   homeBtn.textContent = "Home";
   homeLi.appendChild(homeBtn);
   list.appendChild(homeLi);
   const menuLi = document.createElement("li");
   const menuBtn = document.createElement("button");
-  menuBtn.id = "homeBtn";
   menuBtn.textContent = "Menu";
   menuLi.appendChild(menuBtn);
   list.appendChild(menuLi);
   const contactLi = document.createElement("li");
   const contactBtn = document.createElement("button");
-  contactBtn.id = "homeBtn";
   contactBtn.textContent = "Contact";
   contactLi.appendChild(contactBtn);
-  list.appendChild(contactBtn);
+  list.appendChild(contactLi);
 
   header.appendChild(heading);
   header.appendChild(list);
@@ -47,4 +46,28 @@ export default function makeHomePage() {
   intro.appendChild(orderBtn);
 
   const introFooter = document.createElement("div");
+  introFooter.className = "intro-footer";
+
+  const location = document.createElement("div");
+  location.className = "location";
+  const locationImage = new Image();
+  locationImage.src = locationIcon;
+  location.appendChild(locationImage);
+  const locationInfo = document.createElement("div");
+  locationInfo.textContent = "2210 Paris Rd, Columbia, MO 65202";
+  location.appendChild(locationInfo);
+  introFooter.appendChild(location);
+
+  const time = document.createElement("div");
+  time.className = "time";
+  const timeImage = new Image();
+  timeImage.src = timeIcon;
+  time.appendChild(timeImage);
+  const timeInfo = document.createElement("div");
+  timeInfo.textContent = "Mon-Fri: 8:00am-5:00pm";
+  time.appendChild(timeInfo);
+  introFooter.appendChild(time);
+  intro.appendChild(introFooter);
+
+  content.appendChild(intro);
 }
